d1258bea0ad6625bcde6795a610393fc9899da40
Gia Nhuan <gianhuan1211@gmail.com>
2018-07-27 01:22:58 +0700

    27072018 commit

--- crawl.js
+++ crawl.js
@@ -3,24 +3,14 @@ var   fs = require('fs');
 var axios = require('axios');
 var url = 'https://viblo.asia/api/post/';
 var content ="test save file\r\n";
-fs.readFile('file/write.txt', 'utf8', function(err,data){
-    let url = 'https://viblo.asia/api/posts/';//https://viblo.asia/api/posts/maGK7jMD5j2
-    if(err) throw err;
-    else {
-        console.log('read file success!');
-        data=data.split('\r\n');
-        console.log(typeof data);
-         for(var value of data){
-            console.log(url + value);
-            axios.get(url+value).then(function(response){
-                console.log(response.data.post.data.contents);
-            }).catch(function(err){
-                console.log(err)
-            })
-            break;
-        }
-    }
-});
+var data;
+
+step2GetDataById();
+// fs.readFile('file/data-viblo.json', (err, data) => {
+//   if (err) throw err;
+//   data = JSON.parse(data);
+//   console.log(data);
+// });
 
 (async() => {
     // const browser = await pt.launch({
@@ -31,6 +21,7 @@ fs.readFile('file/write.txt', 'utf8', function(err,data){
     //step1GetAllIdPost(page);
 
 
+
     // const response = await Promise.all([
     //   page.waitForNavigation(waitOptions),
     //   page.click('.pagination a[href*="/page=2"]', clickOptions),
@@ -39,7 +30,54 @@ fs.readFile('file/write.txt', 'utf8', function(err,data){
     //await page.screenshot({path:'example.png'});
     //await browser.close();
 })();
+async function step2GetDataById(){
+    fs.readFile('file/write.txt', 'utf8', function(err,data){
+        let url = 'https://viblo.asia/api/posts/';//https://viblo.asia/api/posts/maGK7jMD5j2
+        if(err) throw err;
+        else {
+            data = data.split('\r\n');
+        }
+        if(!data) return;
+        var jsonFile = {
+            article: []
+        };
+        id=0;
+        let result;
+        let func = [];
+        console.log(data.length);
+        data = data.slice(0,10);
 
+        for(var value of data){
+            //console.log(url + value);
+            id++;
+            result = axios.get(url+value).then(function(response){
+                console.log(id);
+                jsonFile.article.push({
+                    id: id,
+                    title: response.data.post.data.title,
+                    category_id: response.data.post.data.category_id,
+                    contents: response.data.post.data.contents,
+                    is_published: response.data.post.data.is_published,
+                    tags: response.data.post.data.tags,
+                    create_at: response.data.post.data.updated_at,
+                    update_at: response.data.post.data.updated_at,
+                    published_at: response.data.post.data.published_at
+                });
+                if(id=10){
+                    let contentJson = JSON.stringify(jsonFile);
+                    fs.writeFile('file/data-viblo.json', contentJson, 'utf8', function(err){
+                        if(err) throw err;
+                        else console.log('write json file success!');
+                    });
+                }
+            }).catch(function(err){
+                console.log(err)
+            })
+        }
+        //const resultPro = await Promise.all(result);
+
+    });
+}
 async function step1GetAllIdPost(page){
     await page.goto('https://viblo.asia/',{waitUntil: 'load'});
     //let article = document.querySelectorAll('.post-title--inline');

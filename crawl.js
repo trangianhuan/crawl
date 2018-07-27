565c75eeb8b9e96e9490388550dcfb54860d0c3c
trangianhuan <gianhuan1211@gmail.com>
2018-07-27 17:25:19 +0700

    Add data crawl viblo

--- crawl.js
+++ crawl.js
@@ -6,36 +6,25 @@ var content ="test save file\r\n";
 var data;
 
 step2GetDataById();
-// fs.readFile('file/data-viblo.json', (err, data) => {
-//   if (err) throw err;
-//   data = JSON.parse(data);
-//   console.log(data);
-// });
 
-(async() => {
-    // const browser = await pt.launch({
-    //     headless: false,
-    //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    //   });
-    // const page = await browser.newPage();
-    //step1GetAllIdPost(page);
+async function getArticle(id) {
+    let url = 'https://viblo.asia/api/posts/';
+    var result = await fetch(url+value, {
+        method: 'GET'
+    }).then(rs => rs.json());
 
+    return result.value.map(vl => {
+        return { title: vl.title, contents: vl.contents };
+    });
+}
 
-
-    // const response = await Promise.all([
-    //   page.waitForNavigation(waitOptions),
-    //   page.click('.pagination a[href*="/page=2"]', clickOptions),
-    // ]);
-    // console.log(response);
-    //await page.screenshot({path:'example.png'});
-    //await browser.close();
-})();
 async function step2GetDataById(){
-    fs.readFile('file/write.txt', 'utf8', function(err,data){
-        let url = 'https://viblo.asia/api/posts/';//https://viblo.asia/api/posts/maGK7jMD5j2
+    fs.readFile('file/write.txt', 'utf8', async function(err,data){
+        let url = 'https://viblo.asia/api/posts/';
+        //https://viblo.asia/api/posts/maGK7jMD5j2
         if(err) throw err;
         else {
-            data = data.split('\r\n');
+            data = data.split('\n');
         }
         if(!data) return;
         var jsonFile = {
@@ -43,14 +32,14 @@ async function step2GetDataById(){
         };
         id=0;
         let result;
-        let func = [];
+
+        //data = data.slice(0,1);
         console.log(data.length);
-        data = data.slice(0,10);
 
         for(var value of data){
-            //console.log(url + value);
+            console.log(url + value);
             id++;
-            result = axios.get(url+value).then(function(response){
+            result = await axios.get(url+value).then(function(response){
                 console.log(id);
                 jsonFile.article.push({
                     id: id,
@@ -63,69 +52,69 @@ async function step2GetDataById(){
                     update_at: response.data.post.data.updated_at,
                     published_at: response.data.post.data.published_at
                 });
-                if(id=10){
+                if([500,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000,13000].indexOf(id) >= 0){
                     let contentJson = JSON.stringify(jsonFile);
-                    fs.writeFile('file/data-viblo.json', contentJson, 'utf8', function(err){
+                    fs.writeFile('file/data-viblo'+id+'.json', contentJson, 'utf8', function(err){
                         if(err) throw err;
-                        else console.log('write json file success!');
+                        else console.log('write json '+id+' file success!');
                     });
                 }
             }).catch(function(err){
                 console.log(err)
             })
         }
-        //const resultPro = await Promise.all(result);
+
+        let contentJson = JSON.stringify(jsonFile);
+        fs.writeFile('file/data-viblo.json', contentJson, 'utf8', function(err){
+            if(err) throw err;
+            else console.log('write all json success!');
+        });
 
     });
 }
+
 async function step1GetAllIdPost(page){
     await page.goto('https://viblo.asia/',{waitUntil: 'load'});
     //let article = document.querySelectorAll('.post-title--inline');
-    let imgLinks = await page.evaluate(() => {
-        let imgElements = document.querySelectorAll('.post-title--inline h3 a');
-        imgElements = [...imgElements];
-        let imgLinks = imgElements.map(i => {
-            return i.getAttribute('href').split('-').pop();
-        });
-        return imgLinks;
-    });
-
-    console.log(imgLinks);
-    if(imgLinks){
-        await fs.appendFile('file/write.txt', imgLinks.join("\r\n").concat("\r\n"), 'utf8', function(err){
-            if(err) {
-                console.log('loi roi!! ');
-                throw err;
-            }
-            else console.log('write file success!');
-        });
-    }
-
-    let navigationPromise = page.waitForNavigation();
-    await page.click('.pagination a[href*="/?page=2"]'); // Clicking the link will indirectly cause a navigation
-    await navigationPromise; // The nvigationPromise resolves after navigation has finished
-
-    for (var i = 3; i < 700; i++) {
-        console.log('page=',i);
-        imgLinks = await page.evaluate(() => {
+    for (var i = 2; i < 700; i++) {
+        console.log('page=', i - 1);
+        articles = await page.evaluate(() => {
             let imgElements = document.querySelectorAll('.post-title--inline h3 a');
             imgElements = [...imgElements];
-            let imgLinks = imgElements.map(i => {
+            let articles = imgElements.map(i => {
                 return i.getAttribute('href').split('-').pop();
             });
-            return imgLinks;
+            return articles;
         });
 
-        if(imgLinks){
-            await fs.appendFile('file/write.txt', imgLinks.join("\r\n").concat("\r\n"), 'utf8', function(err){
+        if(articles){
+            await fs.appendFile('file/write.txt', articles.join("\r\n").concat("\r\n"), 'utf8', function(err){
                 if(err) throw err;
-                else console.log('write file success!');
+                else console.log('write id file success!');
             });
         }
 
         navigationPromise = page.waitForNavigation();
         await page.click('.pagination a[href*="/?page='+i+'"]'); // Clicking the link will indirectly cause a navigation
         await navigationPromise; // The navigationPromise resolves after navigation has finished
-
     }
 }
+
+
+//(async() => {
+    // const browser = await pt.launch({
+    //     headless: false,
+    //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    //   });
+    // const page = await browser.newPage();
+    //step1GetAllIdPost(page);
+    //
+    //
+    // const response = await Promise.all([
+    //   page.waitForNavigation(waitOptions),
+    //   page.click('.pagination a[href*="/page=2"]', clickOptions),
+    // ]);
+    // console.log(response);
+    //await page.screenshot({path:'example.png'});
+    //await browser.close();
+//})();
